1710e983cc2e88cd64e98b2d1c658aa35e322343
Hemanya Sharma <hemanya.sharma@zoho.com>
2023-08-06 18:19:43 -0400

    Update installer

--- install.sh
+++ install.sh
@@ -13,27 +13,26 @@ printf "Make sure you have all the GPU packages needed along with git, python an
 
 
 # Installer
-text_install () {
+main_install () {
     printf "Cloning ComfyUI git repo...\n"
     git clone https://github.com/comfyanonymous/ComfyUI.git
     cd ComfyUI || exit
 
-    printf "Now setting a Python virtualenv...\n"
+    printf "Setting a Python virtualenv...\n"
     python -m venv sdg
     source sdg/bin/activate
-
     printf "Installing dependencies in python venv...\n"
     pip install torch torchvision torchaudio --extra-index-url "$1"
     pip install -r requirements.txt
 
-    printf "Now copying launch script...\n"
-    cp ../launch.sh launch.sh
-    chmod +x launch.sh
-    printf "Use this to easily launch ComfyUI.\n"
+    cp ../launch.sh comfyui
+    chmod +x comfyui
+    printf "Invoke 'comfyui' inside the 'ComfyUI' folder to easily launch it.\n"
 
-    printf "Now install a checkpoint inside models/checkpoints/ to get started.\n"
+    printf "You will need to install a checkpoint inside ComfyUI/models/checkpoints/ to get started.\n"
 
 }
+
 
 # Pre-install checks
 
@@ -46,15 +45,16 @@ fi
 
 if [ "$1" = "--amd" ]
 then
-    printf "Set to AMD GPU.\n"
+    printf "Installing for AMD GPU.\n"
     printf "if you have a RDNA2 or 3 card, check README.\n"
-    text_install $amd_gpu_repo
+    main_install $amd_gpu_repo
 elif [ "$1" = "--nvidia" ]
 then
-    printf "Set to Nvidia GPU.\n"
-    text_install $nvidia_gpu_repo
+    printf "Installing for Nvidia GPU.\n"
+    main_install $nvidia_gpu_repo
 else
     printf "\033[0;31mError:\033[0m Please specify if you want to install for AMD or Nvidia GPU.\n"
     printf "Use either --amd or --nvidia as an argument.\n"
     exit
+    #GPU=$(zenity --list --title="Select your GPU" --column="Vendor" AMD Nvidia 2>&1)
 fi
