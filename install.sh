d6e8071ecec516fa433b29ee641606268762afde
Hemanya Sharma <hemanya.sharma@zoho.com>
2024-01-03 17:35:00 -0500

    Make text fancy

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-
-
 # Pip packages depend on GPU vendor. You might want to try using an updated or nightly version here if ComfyUI is not working for you. 
 readonly pip_amd="torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.6"
 readonly pip_nvidia="torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121 xformers"
@@ -10,22 +8,21 @@ readonly comfyui_repo="https://github.com/comfyanonymous/ComfyUI.git" # ComfyUI 
 readonly comfyui_folder_name="ComfyUI" # Folder in which to clone ComfyUI Repo
 
 
-printf "This script will install ComfyUI. Tested on Arch + AMD GPU.\n"
-printf "Make sure you have all the GPU packages needed along with git, python and pip.\n"
 
+printf "Make sure you have all the GPU packages needed along with git, python and pip.\n"
 
 
 
 # Installer
 main_install () {
-    printf "Cloning ComfyUI git repo...\n"
+    printf "\033[0;33mInfo:\033[m Cloning ComfyUI git repo...\n"
     git clone "$comfyui_repo" "$comfyui_folder_name"
     cd "$comfyui_folder_name" || exit
 
-    printf "Setting up a Python venv...\n"
+    printf "\033[0;33mInfo:\033[m Setting up a Python venv...\n"
     python -m venv comfy-venv
     source comfy-venv/bin/activate
-    printf "Installing dependencies in python venv...\n"
+    printf "\033[0;33mInfo:\033[m Installing dependencies in python venv...\n"
     pip install "$1"
     pip install -r requirements.txt
 
@@ -60,24 +57,24 @@ fi
 # GPU Vendors
 if [ "$1" = "--amd" ]
 then
-    printf "Installing for AMD GPU.\n"
-    printf "if you have a RDNA2 or 3 card, check README.\n"
+    printf "\033[0;33mInfo:\033[m Installing for \033[0;31mAMD\033[m GPU...\n"
+    printf "if you have a RDNA2 or 3 card, you might want to check the README.\n"
     main_install $amd_gpu_repo
 elif [ "$1" = "--nvidia" ]
 then
-    printf "Installing for Nvidia GPU.\n"
+    printf "\033[0;33mInfo:\033[m Installing for \033[0;32mNvidia\033[m GPU...\n"
     main_install $nvidia_gpu_repo
 else
-    printf "\033[0;31mError:\033[0m Please specify if you want to install for AMD or Nvidia GPU.\n"
-    printf "Use either --amd or --nvidia as an argument.\n"
-    printf "You can also specify --make-menu-entry after your GPU vendor to make a menu entry for ComfyUI.\n"
+    printf "\033[0;31mError:\033[m Please specify if you want to install for AMD or Nvidia GPU.\n"
+    printf "    - Use either \033[0;31m--amd\033[m or \033[0;32m--nvidia\033[m as an argument.\n"
+    printf "    - You can also specify --make-menu-entry after your GPU vendor to make a menu entry for ComfyUI.\n"
     exit
 fi
 
 if [ "$2" = "--make-menu-entry" ]
 then
-    printf "Making a menu entry for ComfyUI...\n"
+    printf "\033[0;33mInfo:\033[m Making a menu entry for ComfyUI...\n"
     menu_icon
 fi
 
-printf "\n*Script Exited*\n"
+printf "\n\033[0;33mInfo:\033[m Script Exited\n"
