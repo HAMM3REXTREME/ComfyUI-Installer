cb820327d19947f149e6b05810913dbaaafb406c
Hemanya Sharma <hemanya.sharma@zoho.com>
2024-10-20 10:40:11 -0400

    Add information about args and env vars

--- launch.sh
+++ launch.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # ComfyUI-Installer, launcher script.
 
-args=$@ # User passed command line args
+set -e
+args=$@ # Command line arguments passed by the user
 source venv/bin/activate
 cd ComfyUI
 
-# Add any env vars or custom args below
-HSA_OVERRIDE_GFX_VERSION=10.3.0 python main.py $args
+# Add any environment variables or custom arguments below. For example:
+# HSA_OVERRIDE_GFX_VERSION=10.3.0 python main.py --lowvram --preview-method auto --use-split-cross-attention $args
+python main.py $args
