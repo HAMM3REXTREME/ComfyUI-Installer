fe1858cc11bb88268c69fe005bc9ad7335844478
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-04-15 03:28:31 +0200

    modified:   .gitignore 	modified:   install.sh

--- install.sh
+++ install.sh
@@ -249,6 +249,18 @@ EOF
     cp "$COMFYUI_INSTALLER_DIR/scripts/ComfyUI.desktop" ~/.local/share/applications/ComfyUI.desktop
     chmod +x ~/.local/share/applications/ComfyUI.desktop
 
+    printf "[*] Adding ComfyUI to your desktop.\n"
+    exec_path="$COMFYUI_INSTALLER_DIR/scripts/run_gpu.sh"   # Launch script
+    icon_path="$COMFYUI_INSTALLER_DIR/graphics/comfyui.svg" # ComfyUI Icon
+    desktop-file-install \
+        --dir="$HOME/.local/share/applications/" \
+        --set-key=Path \
+        --set-value="$PWD" \
+        --set-key=Exec \
+        --set-value="$exec_path" \
+        --set-icon="$icon_path" \
+        "$COMFYUI_INSTALLER_DIR/scripts/ComfyUI.desktop"
+
     printf "[*] Creating %s/scripts/ComfyUIMini.desktop" "$COMFYUI_INSTALLER_DIR"
     cat <<EOF >"$COMFYUI_INSTALLER_DIR/scripts/ComfyUIMini.desktop"
 [Desktop Entry]
@@ -263,18 +275,6 @@ NoDisplay=false
 EOF
     cp "$COMFYUI_INSTALLER_DIR/scripts/ComfyUIMini.desktop" ~/.local/share/applications/ComfyUIMini.desktop
     chmod +x ~/.local/share/applications/ComfyUIMini.desktop
-
-    printf "[*] Adding ComfyUI to your desktop.\n"
-    exec_path="$COMFYUI_INSTALLER_DIR/scripts/run_gpu.sh"   # Launch script
-    icon_path="$COMFYUI_INSTALLER_DIR/graphics/comfyui.svg" # ComfyUI Icon
-    desktop-file-install \
-        --dir="$HOME/.local/share/applications/" \
-        --set-key=Path \
-        --set-value="$PWD" \
-        --set-key=Exec \
-        --set-value="$exec_path" \
-        --set-icon="$icon_path" \
-        "$COMFYUI_INSTALLER_DIR/scripts/ComfyUI.desktop"
 
     printf "[*] Adding ComfyUIMini to your desktop.\n"
     exec_path="$COMFYUI_DIR/custom_nodes/ComfyUIMini/scripts/start.sh" # Launch script
